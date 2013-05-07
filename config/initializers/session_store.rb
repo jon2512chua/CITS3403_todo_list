02e860af73ca4645d149e50ca724b5e63f93b8a8
Jonathan Chua <me@jonathanchua.com>
2013-05-07 13:23:00 +0800

    Settings fixes to get the server up and running.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-CITS3403TodoList::Application.config.session_store :cookie_store, key: '_CITS3403_todo_list_session'
+CITS3403TodoList::Application.config.session_store :cookie_store, :key => '_CITS3403_todo_list_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
