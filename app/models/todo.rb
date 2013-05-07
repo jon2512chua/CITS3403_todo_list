288ee5d1e23523da12a0e783a2151ab24d046dbb
Alvar <20845163@student.uwa.edu.au>
2013-05-07 12:02:51 +0800

    validation of fields for user, todo and todolist\n email regex not functioning properly

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,8 @@
 class Todo < ActiveRecord::Base
   belongs_to :TodoList
   attr_accessible :completed, :content, :due_date, :user_id
+  validates :completed, :presence => true
+  validates :content, :presence => true
+  validates :due_date, :presence => true
+  validates :user_id, :presence => true
 end
--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,6 @@
 class TodoList < ActiveRecord::Base
   has_and_belongs_to_many :users
   attr_accessible :name, :user_id
+  validates :name, :presence => true, :uniqueness => true
+  validates :user_id, :presence => true
 end
