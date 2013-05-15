f7af62acc2a02e7124e714e312425d0fb238c450
Alvar <20845163@student.uwa.edu.au>
2013-05-15 12:30:38 +0800

    Finished Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe "Static pages" do
+	let(:base_title) { "My Todos" }
+
+  describe "Home page" do
+
+    it "should have the h1 'My Todos'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'My Todos')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Home")
+    end    
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title',
+                        :text => "#{base_title} | Help")
+    end        
+  end  
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About')
+    end
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title',
+                        :text => "#{base_title} | About")
+    end        
+  end    
+end
