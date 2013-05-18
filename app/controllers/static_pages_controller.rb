f3cb5ef4b2eb3066a80f7a7708854d329dcbac42
xiaofan2406 <xiaofan2406@gmail.com>
2013-05-18 12:23:02 +0800

    fix a minor bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     @user = current_user if signed_in?
     @item = current_user.items.build if signed_in?
-    @items = @user.items.paginate(page: params[:page])
+    @items = @user.items.paginate(page: params[:page]) if signed_in?
   end
 
   def help
