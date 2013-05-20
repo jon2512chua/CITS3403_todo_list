e43b0f706c815b8d6355dbb971b198e843481bf3
Patrick Ong <p.ongsh@gmail.com>
2013-05-20 14:40:17 +0800

    Added completed to the items model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518043718) do
+ActiveRecord::Schema.define(:version => 20130520063720) do
 
   create_table "items", :force => true do |t|
     t.string   "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(:version => 20130518043718) do
     t.integer  "user_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.boolean  "completed"
   end
 
   add_index "items", ["user_id", "created_at"], :name => "index_items_on_user_id_and_created_at"
