6cb680c0a5d93500909f9348cc0ba6f3704a1f92
xiaofan2406 <xiaofan2406@gmail.com>
2013-05-22 14:02:00 +0800

    add date validation must before today

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,9 +2,16 @@ class Item < ActiveRecord::Base
   attr_accessible :content, :name, :due_date
   belongs_to :user
 
-  validates :content, length: { maximum: 140 }  
-  validates :name, presence: true, length: { maximum: 80 } 
+  validates :content, length: { maximum: 140 }
+  validates :name, presence: true, length: { maximum: 80 }
   validates :user_id, presence: true
+  validate :due_date_cannot_be_in_the_past
 
-  default_scope order: 'items.created_at DESC'  
+  def due_date_cannot_be_in_the_past
+    if !due_date.blank? and due_date < Date.today
+      errors.add(:due_date, "can't be in the past")
+    end
+  end
+
+  default_scope order: 'items.created_at DESC'
 end
